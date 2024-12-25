b773083e7de82e026f7bfb3108190ad026d8f268
zahid-official <zahid.official8@gmail.com>
2024-12-26 01:03:02 +0600

    Setup

---
+++ index.js
@@ -0,0 +1,51 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.rjxsn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // await client.connect();
+
+    // database collection
+    const database = client.db("booksDB");
+    const booksCollection = database.collection("books");
+    const borrowedCollection = database.collection("borrowed");
+
+    // read 
+    app.get("/", (req, res) => {
+      res.send("Server Connected");
+    });
+
+
+
+
+
+
+  } finally {
+  }
+}
+run().catch(console.log);
+
+app.listen(port, () => {
+  console.log(`Server Running on...: ${port}`);
+});
